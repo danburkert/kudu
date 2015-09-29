danburkert/kudu-old

Fix time and __NR_gettid in gutil
Change-Id: If68e37e6bb96b30e4c4457eb6eb4b417359e96bc

src/kudu/gutil/walltime.h
@@ -24,6 +24,11 @@
 #include <string>
 using std::string;
 
+#if defined(__APPLE__)
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif  // defined(__APPLE__)
+
 #include "kudu/gutil/integral_types.h"
 
 typedef double WallTime;
@@ -53,7 +58,17 @@ typedef int64 MicrosecondsInt64;
 // Returns the time since the Epoch measured in microseconds.
 inline MicrosecondsInt64 GetCurrentTimeMicros() {
   timespec ts;
+#if defined(linux)
   clock_gettime(CLOCK_REALTIME, &ts);
+#elif defined(__APPLE__)
+  clock_serv_t cclock;
+  mach_timespec_t mts;
+  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+  clock_get_time(cclock, &mts);
+  mach_port_deallocate(mach_task_self(), cclock);
+  ts.tv_sec = mts.tv_sec;
+  ts.tv_nsec = mts.tv_nsec;
+#endif  // defined(linux)
   return ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
 }
 

danburkert/kudu

src/kudu/gutil/walltime.h
@@ -19,6 +19,11 @@
 #include <string>
 using std::string;
 
+#if defined(__APPLE__)
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif  // defined(__APPLE__)
+
 #include "kudu/gutil/integral_types.h"
 
 typedef double WallTime;
@@ -49,27 +54,53 @@ WallTime WallTime_Now();
 typedef int64 MicrosecondsInt64;
 
 namespace walltime_internal {
+
+#if defined(linux)
 inline MicrosecondsInt64 GetClockTimeMicros(clockid_t clock) {
   timespec ts;
   clock_gettime(clock, &ts);
   return ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
 }
+
+#elif defined(__APPLE__)
+inline MicrosecondsInt64 GetClockTimeMicros() {
+  clock_serv_t cclock;
+  mach_timespec_t ts;
+  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+  clock_get_time(cclock, &ts);
+  mach_port_deallocate(mach_task_self(), cclock);
+  return ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
+}
+#endif  // defined(linux)
+
 } // namespace walltime_internal
 
 // Returns the time since the Epoch measured in microseconds.
 inline MicrosecondsInt64 GetCurrentTimeMicros() {
+#if defined(linux)
   return walltime_internal::GetClockTimeMicros(CLOCK_REALTIME);
+#elif defined(__APPLE__)
+  return walltime_internal::GetClockTimeMicros();
+#endif  // defined(linux)
 }
 
 // Returns the time since some arbitrary reference point, measured in microseconds.
 // Guaranteed to be monotonic (and therefore useful for measuring intervals)
 inline MicrosecondsInt64 GetMonoTimeMicros() {
+#if defined(linux)
   return walltime_internal::GetClockTimeMicros(CLOCK_MONOTONIC);
+#elif defined(__APPLE__)
+  return walltime_internal::GetClockTimeMicros();
+#endif  // defined(linux)
 }
 
 // Returns the time spent in user CPU on the current thread, measured in microseconds.
 inline MicrosecondsInt64 GetThreadCpuTimeMicros() {
+#if defined(linux)
   return walltime_internal::GetClockTimeMicros(CLOCK_THREAD_CPUTIME_ID);
+#elif defined(__APPLE__)
+  return walltime_internal::GetClockTimeMicros();
+#endif  // defined(linux)
 }
 
 // A CycleClock yields the value of a cycle counter that increments at a rate