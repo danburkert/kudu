1b086e829daba9566b57cb51035521420f532d1c
Dan Burkert <dan@danburkert.com>
2016-03-03 22:03:24 -0800

    Add table creation APIs

--- src/kudu/client/c.cc
+++ src/kudu/client/c.cc
@@ -38,10 +38,29 @@ using std::vector;
 using kudu::client::KuduClient;
 using kudu::client::KuduClientBuilder;
 using kudu::client::KuduColumnSchema;
+using kudu::client::KuduColumnSpec;
+using kudu::client::KuduColumnStorageAttributes;
 using kudu::client::KuduSchema;
+using kudu::client::KuduSchemaBuilder;
+using kudu::client::KuduTableCreator;
 using kudu::client::sp::shared_ptr;
 using kudu::MonoDelta;
 using kudu::Status;
+
+namespace {
+  string slice_to_string(kudu_slice slice) {
+    return string(reinterpret_cast<const char*>(slice.data), slice.len);
+  }
+
+  vector<string> slice_list_to_vector(kudu_slice_list list) {
+    vector<string> v;
+    v.reserve(list.len);
+    for (int i = 0; i < list.len; i++) {
+      v.emplace_back(slice_to_string(list.data[i]));
+    }
+    return v;
+  }
+} // anonymous namespace
 
 extern "C" {
 
@@ -54,6 +73,8 @@ struct kudu_column_schema {
   kudu_column_schema(KuduColumnSchema column) : column_(move(column)) {}
   KuduColumnSchema column_;
 };
+
+struct kudu_schema_builder { KuduSchemaBuilder builder_; };
 
 ////////////////////////////////////////////////////////////////////////////////
 // Kudu Status
@@ -103,8 +124,7 @@ void kudu_client_builder_destroy(kudu_client_builder* builder) {
 
 void kudu_client_builder_add_master_server_addr(kudu_client_builder* builder,
                                                 kudu_slice addr) {
-  builder->builder_.add_master_server_addr(string(reinterpret_cast<const char*>(addr.data),
-                                                  addr.len));
+  builder->builder_.add_master_server_addr(slice_to_string(addr));
 }
 
 void kudu_client_builder_clear_master_server_addrs(kudu_client_builder* builder) {
@@ -203,6 +223,75 @@ kudu_compression_type kudu_column_schema_compression_type(const kudu_column_sche
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Kudu Schema Builder
+////////////////////////////////////////////////////////////////////////////////
+
+kudu_schema_builder* kudu_schema_builder_create() {
+  return new kudu_schema_builder;
+}
+
+void kudu_schema_builder_destroy(kudu_schema_builder* builder) {
+  delete builder;
+}
+
+kudu_column_schema_builder* kudu_schema_builder_add_column(kudu_schema_builder* builder,
+                                                           kudu_slice name) {
+  return reinterpret_cast<kudu_column_schema_builder*>(
+      builder->builder_.AddColumn(slice_to_string(name)));
+}
+
+void kudu_schema_builder_set_primary_key_columns(kudu_schema_builder* builder,
+                                                 kudu_slice_list column_names) {
+  builder->builder_.SetPrimaryKey(slice_list_to_vector(column_names));
+}
+
+const kudu_status* kudu_schema_builder_build(kudu_schema_builder* builder, kudu_schema** schema) {
+  unique_ptr<kudu_schema> s(new kudu_schema);
+  RETURN_NOT_OK_C(builder->builder_.Build(&s->schema_));
+  *schema = s.release();
+  return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Kudu Column Schema Builder
+//
+// kudu_column_schema_builder is an uninhabited type. Instead, we use a
+// KuduColumnSpec.
+////////////////////////////////////////////////////////////////////////////////
+
+void kudu_column_schema_builder_data_type(kudu_column_schema_builder* builder,
+                                          kudu_data_type data_type) {
+  auto spec = reinterpret_cast<KuduColumnSpec*>(builder);
+  spec->Type(static_cast<KuduColumnSchema::DataType>(data_type));
+}
+
+void kudu_column_schema_builder_encoding_type(kudu_column_schema_builder* builder,
+                                              kudu_encoding_type encoding_type) {
+  auto spec = reinterpret_cast<KuduColumnSpec*>(builder);
+  spec->Encoding(static_cast<KuduColumnStorageAttributes::EncodingType>(encoding_type));
+}
+
+void kudu_column_schema_builder_compression_type(kudu_column_schema_builder* builder,
+                                                 kudu_compression_type compression_type) {
+  auto spec = reinterpret_cast<KuduColumnSpec*>(builder);
+  spec->Compression(static_cast<KuduColumnStorageAttributes::CompressionType>(compression_type));
+}
+
+void kudu_column_schema_builder_block_size(kudu_column_schema_builder* builder,
+                                           int32_t block_size) {
+  auto spec = reinterpret_cast<KuduColumnSpec*>(builder);
+  spec->BlockSize(block_size);
+}
+
+void kudu_column_schema_builder_nullable(kudu_column_schema_builder* builder, int32_t nullable) {
+  if (nullable) {
+    reinterpret_cast<KuduColumnSpec*>(builder)->Nullable();
+  } else {
+    reinterpret_cast<KuduColumnSpec*>(builder)->NotNull();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Kudu Client
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -223,10 +312,64 @@ const kudu_status* kudu_client_table_schema(const kudu_client* client,
                                             kudu_slice table_name,
                                             kudu_schema** schema) {
   unique_ptr<kudu_schema> s(new kudu_schema);
-  RETURN_NOT_OK_C(client->client_->GetTableSchema(
-        string(reinterpret_cast<const char*>(table_name.data), table_name.len),
-        &s.get()->schema_));
+  RETURN_NOT_OK_C(client->client_->GetTableSchema(slice_to_string(table_name), &s.get()->schema_));
   *schema = s.release();
   return nullptr;
 }
+
+kudu_table_creator* kudu_client_new_table_creator(kudu_client* client) {
+  return reinterpret_cast<kudu_table_creator*>(client->client_->NewTableCreator());
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Kudu Table Creator
+//
+// kudu_table_creator is an uninhabited type. Instead, we use a
+// KuduTableCreator.
+////////////////////////////////////////////////////////////////////////////////
+
+void kudu_table_creator_destroy(kudu_table_creator* creator) {
+  delete reinterpret_cast<KuduTableCreator*>(creator);
+}
+
+void kudu_table_creator_table_name(kudu_table_creator* creator,
+                                   kudu_slice table_name) {
+  reinterpret_cast<KuduTableCreator*>(creator)->table_name(slice_to_string(table_name));
+}
+
+void kudu_table_creator_schema(kudu_table_creator* creator, const kudu_schema* schema) {
+  reinterpret_cast<KuduTableCreator*>(creator)->schema(&schema->schema_);
+}
+
+void kudu_table_creator_add_hash_partitions(kudu_table_creator* creator,
+                                            kudu_slice_list columns,
+                                            int32_t num_buckets,
+                                            int32_t seed) {
+  reinterpret_cast<KuduTableCreator*>(creator)->add_hash_partitions(slice_list_to_vector(columns),
+                                                                    num_buckets,
+                                                                    seed);
+}
+
+void kudu_table_creator_set_range_partition_columns(kudu_table_creator* creator,
+                                                    kudu_slice_list columns) {
+  reinterpret_cast<KuduTableCreator*>(creator)
+      ->set_range_partition_columns(slice_list_to_vector(columns));
+}
+
+void kudu_table_creator_num_replicas(kudu_table_creator* creator, int32_t num_replicas) {
+  reinterpret_cast<KuduTableCreator*>(creator)->num_replicas(num_replicas);
+}
+
+void kudu_table_creator_timeout(kudu_table_creator* creator, int64_t timeout_ms) {
+  reinterpret_cast<KuduTableCreator*>(creator)->timeout(MonoDelta::FromMilliseconds(timeout_ms));
+}
+
+void kudu_table_creator_wait(kudu_table_creator* creator, int32_t wait) {
+  reinterpret_cast<KuduTableCreator*>(creator)->wait(wait);
+}
+
+const kudu_status* kudu_table_creator_create(kudu_table_creator* creator) {
+  return Status::into_kudu_status(reinterpret_cast<KuduTableCreator*>(creator)->Create());
+}
+
+} // extern "C"
