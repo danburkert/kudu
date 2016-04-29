7da852a42185b35af76dbb07b85e075d35a1978f
Jean-Daniel Cryans <jdcryans@apache.org>
2016-04-29 08:55:10 -0700

    First cut at querying
    Change-Id: Idf29330183384d81a642ae9a63c57018b343b78d

--- java/kudu-ts/src/main/java/org/kududb/ts/KuduTSTable.java
+++ java/kudu-ts/src/main/java/org/kududb/ts/KuduTSTable.java
@@ -23,25 +23,41 @@ import com.google.common.annotations.VisibleForTesting;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.stumbleupon.async.Callback;
+import com.stumbleupon.async.Deferred;
 import org.kududb.annotations.InterfaceAudience;
 import org.kududb.annotations.InterfaceStability;
 import org.kududb.client.AsyncKuduClient;
+import org.kududb.client.AsyncKuduScanner;
+import org.kududb.client.KuduPredicate;
 import org.kududb.client.KuduTable;
+import org.kududb.client.RowResult;
+import org.kududb.client.RowResultIterator;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @InterfaceAudience.Public
 @InterfaceStability.Evolving
 @NotThreadSafe
 public class KuduTSTable {
 
-  private final AsyncKuduClient client;
   private final String tableName;
   private final KuduTSSchema schema;
 
+  private final AsyncKuduClient client;
   private final KuduTable metricsTable;
   private final KuduTable tagsetsTable;
   private final KuduTable tagsTable;
 
   private final TagsetCache tagsetCache;
+  private final List<Integer> metricsQueryProjection;
+  private final List<Integer> tagsQueryProjection;
 
   public String getTableName() {
     return tableName;
@@ -69,5 +85,145 @@ public class KuduTSTable {
     this.tagsetsTable = tagsetsTable;
     this.tagsTable = tagsTable;
     this.tagsetCache = new TagsetCache(client, schema, tagsetsTable, tagsTable);
+    this.metricsQueryProjection = Lists.newArrayList(2, 3);
+    this.tagsQueryProjection = Lists.newArrayList(2);
+  }
+
+  public QueryResult queryMetrics(long startTimestampMs,
+                                  long endTimestampMs,
+                                  String metricName,
+                                  Map<String, String> tags) throws Exception {
+
+    QueryResult qr  = new QueryResult(metricName);
+    // If the user doesn't provide tags, then we want all the data points for the specified metric.
+    Set<Long> tagsetIDs;
+    if (tags.isEmpty()) {
+      tagsetIDs = new HashSet<>();
+    } else {
+      tagsetIDs = getTagsetIdsForTags(tags);
+      if (tagsetIDs.isEmpty()) {
+        // We know we won't find any metrics.
+        return qr;
+      }
+    }
+
+    List<Deferred<Void>> deferreds = new ArrayList<>(tagsetIDs.size());
+
+    // Launch scanners for all the tagsetIDs.
+    for (Long tagsetId : tagsetIDs) {
+      KuduPredicate metricPred = KuduPredicate.newComparisonPredicate(
+          tagsTable.getSchema().getColumnByIndex(0),
+          KuduPredicate.ComparisonOp.EQUAL, metricName);
+
+      KuduPredicate tagsetIdPred = KuduPredicate.newComparisonPredicate(
+          tagsTable.getSchema().getColumnByIndex(1),
+          KuduPredicate.ComparisonOp.EQUAL, tagsetId);
+
+      KuduPredicate startTimestampPred = KuduPredicate.newComparisonPredicate(
+          tagsTable.getSchema().getColumnByIndex(2),
+          KuduPredicate.ComparisonOp.GREATER, startTimestampMs);
+
+      KuduPredicate endTimestampPred = KuduPredicate.newComparisonPredicate(
+          tagsTable.getSchema().getColumnByIndex(2),
+          KuduPredicate.ComparisonOp.LESS, endTimestampMs);
+
+      AsyncKuduScanner metricScanner = client.newScannerBuilder(metricsTable)
+          .addPredicate(metricPred)
+          .addPredicate(tagsetIdPred)
+          .addPredicate(startTimestampPred)
+          .addPredicate(endTimestampPred)
+          .setProjectedColumnIndexes(metricsQueryProjection)
+          .build();
+
+      deferreds.add(metricScanner.nextRows().addCallbackDeferring(
+          new MetricsScannerCB(metricScanner)));
+    }
+
+    // TODO need to grab all the tags for each tagsets?
+
+    return qr;
+  }
+
+  private class MetricsScannerCB implements Callback<Deferred<Void>, RowResultIterator> {
+
+    private final AsyncKuduScanner metricsScanner;
+
+    MetricsScannerCB(AsyncKuduScanner metricsScanner) {
+      this.metricsScanner = metricsScanner;
+    }
+
+    @Override
+    public Deferred<Void> call(RowResultIterator rowResults) throws Exception {
+
+      for (RowResult rr : rowResults) {
+        //ids.add(rr.getLong(0));
+      }
+
+      if (metricsScanner.hasMoreRows()) {
+        return metricsScanner.nextRows().addCallbackDeferring(this);
+      }
+      return Deferred.fromResult(null);
+    }
+  }
+
+  private Set<Long> getTagsetIdsForTags(Map<String, String> tags) throws Exception {
+    Preconditions.checkArgument(!tags.isEmpty());
+
+    List<Deferred<Set<Long>>> deferreds = new ArrayList<>(tags.size());
+
+    // Kickoff all the scanners and add them to deferreds.
+    for ( Map.Entry<String, String> entry : tags.entrySet()) {
+      KuduPredicate keyPred = KuduPredicate.newComparisonPredicate(
+          tagsTable.getSchema().getColumnByIndex(0),
+          KuduPredicate.ComparisonOp.EQUAL, entry.getKey());
+
+      KuduPredicate valuePred = KuduPredicate.newComparisonPredicate(
+          tagsTable.getSchema().getColumnByIndex(1),
+          KuduPredicate.ComparisonOp.EQUAL, entry.getValue());
+
+      AsyncKuduScanner tagsScanner = client.newScannerBuilder(tagsTable)
+          .addPredicate(keyPred)
+          .addPredicate(valuePred)
+          .setProjectedColumnIndexes(tagsQueryProjection)
+          .build();
+
+      deferreds.add(tagsScanner.nextRows().addCallbackDeferring(new TagsScannerCB(tagsScanner)));
+    }
+
+    // Do a group wait, collect all the results.
+    ArrayList<Set<Long>> result = Deferred.group(deferreds).join(10000);
+
+    // Get the intersection of all the tags.
+    Set<Long> intersection = new HashSet<>(result.get(0));
+    for (Set<Long> ids : result) {
+      intersection.retainAll(ids);
+
+      // Short-circuit, nothing else is going to happen.
+      if (ids.isEmpty()) {
+        break;
+      }
+    }
+
+    return intersection;
+  }
+
+  private class TagsScannerCB implements Callback<Deferred<Set<Long>>, RowResultIterator> {
+    private final Set<Long> ids = new HashSet<>();
+    private final AsyncKuduScanner tagsScanner;
+
+    TagsScannerCB(AsyncKuduScanner tagsScanner) {
+      this.tagsScanner = tagsScanner;
+    }
+
+    @Override
+    public Deferred<Set<Long>> call(RowResultIterator rowResults) throws Exception {
+      for (RowResult rr : rowResults) {
+        ids.add(rr.getLong(0));
+      }
+      if (tagsScanner.hasMoreRows()) {
+        return tagsScanner.nextRows().addCallbackDeferring(this);
+      }
+      return Deferred.fromResult(ids);
+    }
   }
 }
