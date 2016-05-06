e59c5f6e0fc6e89772c9e9626abc2231ccfd1342
cgeorge <chris.george@rms.com>
2016-05-06 16:43:47 -0600

    Kudu Spark Datasource for insertions and updates
    Change-Id: I74f20a6c17c47f424dfda62854963dc19c3b78c3

--- java/kudu-spark/src/main/scala/org/kududb/spark/kudu/KuduContext.scala
+++ java/kudu-spark/src/main/scala/org/kududb/spark/kudu/KuduContext.scala
@@ -17,12 +17,19 @@
 
 package org.kududb.spark.kudu
 
+import java.util
 import org.apache.hadoop.util.ShutdownHookManager
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.types.{StructField, StructType, DataType, DataTypes}
+import org.kududb.{ColumnSchema, Schema, Type}
 import org.kududb.annotations.InterfaceStability
-import org.kududb.client.{AsyncKuduClient, KuduClient}
+import org.kududb.client.SessionConfiguration.FlushMode
+import org.kududb.client._
+import scala.collection.JavaConverters._
+import java.sql.Timestamp
+
 
 /**
   * KuduContext is a serializable container for Kudu client connections.
@@ -75,4 +82,123 @@ class KuduContext(kuduMaster: String) extends Serializable {
     new KuduRDD(kuduMaster, 1024*1024*20, columnProjection.toArray, Array(),
                 syncClient.openTable(tableName), this, sc)
   }
+
+  /**
+    * Check if kudu table already exists
+    * @param tableName tablename to check
+    * @return true if table exists, false if table does not exist
+    */
+  def tableExists(tableName: String): Boolean = syncClient.tableExists(tableName)
+
+  /**
+    * Delete kudu table
+    * @param tableName tablename to delete
+    * @return DeleteTableResponse
+    */
+  def deleteTable(tableName: String): DeleteTableResponse = syncClient.deleteTable(tableName)
+
+  /**
+    * Creates a kudu table for the given schema. Partitioning can be specified through options.
+    * @param tableName table to create
+    * @param schema struct schema of table
+    * @param keys primary keys of the table
+    * @param options replication and partitioning options for the table
+    */
+  def createTable(tableName: String,
+                  schema: StructType,
+                  keys: Seq[String],
+                  options: CreateTableOptions): KuduTable = {
+    val kuduCols = new util.ArrayList[ColumnSchema]()
+    // add the key columns first, in the order specified
+    for (key <- keys) {
+      val f = schema.fields(schema.fieldIndex(key))
+      kuduCols.add(new ColumnSchema.ColumnSchemaBuilder(f.name, kuduType(f.dataType)).key(true).build())
+    }
+    // now add the non-key columns
+    for (f <- schema.fields.filter(field=> !keys.contains(field.name))) {
+      kuduCols.add(new ColumnSchema.ColumnSchemaBuilder(f.name, kuduType(f.dataType)).nullable(f.nullable).key(false).build())
+    }
+
+    syncClient.createTable(tableName, new Schema(kuduCols), options)
+  }
+
+  /** Map Spark SQL type to Kudu type */
+  def kuduType(dt: DataType) : Type = dt match {
+    case DataTypes.BinaryType => Type.BINARY
+    case DataTypes.BooleanType => Type.BOOL
+    case DataTypes.StringType => Type.STRING
+    case DataTypes.TimestampType => Type.TIMESTAMP
+    case DataTypes.ByteType => Type.INT8
+    case DataTypes.ShortType => Type.INT16
+    case DataTypes.IntegerType => Type.INT32
+    case DataTypes.LongType => Type.INT64
+    case DataTypes.FloatType => Type.FLOAT
+    case DataTypes.DoubleType => Type.DOUBLE
+    case _ => throw new RuntimeException(s"No support for Spark SQL type $dt")
+  }
+
+  /**
+    * Inserts or updates rows in kudu from a dataframe
+    * @param data Dataframe to insert/update
+    * @param tableName Table to perform insertion on
+    * @param overwrite true=update, false=insert
+    */
+  def writeRows(data:DataFrame, tableName:String, overwrite:Boolean): Unit = {
+    data.foreachPartition(iterator => new KuduContext(kuduMaster).writeRows(iterator, tableName, overwrite))
+  }
+
+  /**
+    * Saves partitions of a dataframe into kudu
+    * @param rows rows to insert or update
+    * @param tableName table to insert or update on
+    */
+  def writeRows(rows: Iterator[Row],
+                tableName: String,
+                performAsUpdate : Boolean = false): RowErrorsAndOverflowStatus = {
+    val table: KuduTable = syncClient.openTable(tableName)
+    val session: KuduSession = syncClient.newSession
+    session.setFlushMode(FlushMode.AUTO_FLUSH_BACKGROUND)
+    try {
+      while (rows.hasNext) {
+        val dfRow = rows.next()
+        var operation: Operation = table.newInsert()
+        if (performAsUpdate) {
+          operation = table.newUpdate()
+        }
+        val kuduRow: PartialRow = operation.getRow
+        for (sparkColumn <- dfRow.schema.zipWithIndex) {
+          // It seems to be most efficient to to the idx lookup on kudu side and not spark
+          // this is because 1. dfRow.schema.fieldIndex will return exception for missing column
+          // 2. Any of the kudu rows can throw an exception also, but this is because the underlying
+          // table does not contain that column
+          val sparkIdx = sparkColumn._2
+          val sparkField = sparkColumn._1
+          val dt: DataType = sparkField.dataType
+          val fieldName: String = sparkField.name
+          if (dfRow.isNullAt(sparkIdx)) {
+
+          } else dt match {
+            case DataTypes.StringType => kuduRow.addString(fieldName, dfRow.getAs[String](sparkIdx))
+            case DataTypes.BinaryType => kuduRow.addBinary(fieldName, dfRow.getAs[Array[Byte]](sparkIdx))
+            case DataTypes.BooleanType => kuduRow.addBoolean(fieldName, dfRow.getAs[Boolean](sparkIdx))
+            case DataTypes.ByteType => kuduRow.addInt(fieldName, dfRow.getAs[Byte](sparkIdx))
+            case DataTypes.ShortType => kuduRow.addShort(fieldName, dfRow.getAs[Short](sparkIdx))
+            case DataTypes.IntegerType => kuduRow.addInt(fieldName, dfRow.getAs[Int](sparkIdx))
+            case DataTypes.LongType => kuduRow.addLong(fieldName, dfRow.getAs[Long](sparkIdx))
+            case DataTypes.FloatType => kuduRow.addFloat(fieldName, dfRow.getAs[Float](sparkIdx))
+            case DataTypes.DoubleType => kuduRow.addDouble(fieldName, dfRow.getAs[Double](sparkIdx))
+            case DataTypes.TimestampType => kuduRow.addLong(fieldName, dfRow.getAs[Timestamp](sparkIdx).getTime * 1000)
+            case _ => throw new IllegalArgumentException(s"No support for Spark SQL type $dt")
+          }
+        }
+        session.apply(operation)
+      }
+    } finally {
+      session.close()
+    }
+    session.getPendingErrors
+  }
+
+
+
 }
--- java/kudu-spark/src/test/scala/org/kududb/spark/kudu/DefaultSourceTest.scala
+++ java/kudu-spark/src/test/scala/org/kududb/spark/kudu/DefaultSourceTest.scala
@@ -23,8 +23,10 @@ import java.util.TimeZone
 import org.apache.spark.sql.SQLContext
 import org.junit.Assert._
 import org.junit.runner.RunWith
+import org.kududb.client.CreateTableOptions
 import org.scalatest.{BeforeAndAfter, FunSuite}
 import org.scalatest.junit.JUnitRunner
+import org.apache.spark.sql.functions._
 
 import scala.collection.immutable.IndexedSeq
 
@@ -62,6 +64,102 @@ class DefaultSourceTest extends FunSuite with TestContext with BeforeAndAfter {
     sqlContext.read.options(
       Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu
       .registerTempTable(tableName)
+  }
+
+  test("table creation") {
+    if(kuduContext.tableExists("testcreatetable")) {
+      kuduContext.deleteTable("testcreatetable")
+    }
+
+    val df = sqlContext.read.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu
+
+    kuduContext.createTable("testcreatetable", df.schema, Seq("key"), new CreateTableOptions().setNumReplicas(1))
+
+    df.write.options(
+      Map("kudu.table" -> "testcreatetable", "kudu.master" -> miniCluster.getMasterAddresses)).mode("append").kudu
+
+    val checkDf = sqlContext.read.options(
+      Map("kudu.table" -> "testcreatetable", "kudu.master" -> miniCluster.getMasterAddresses)).kudu
+
+    assert(checkDf.schema === df.schema)
+
+    assertTrue(kuduContext.tableExists("testcreatetable"))
+    assert(checkDf.count == 10)
+    kuduContext.deleteTable("testcreatetable")
+
+    assertFalse(kuduContext.tableExists("testcreatetable"))
+  }
+
+  test("insertion") {
+    val df = sqlContext.read.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu
+    val changedDF = df.limit(1).withColumn("key", df("key").plus(100)).withColumn("c2_s", lit("abc"))
+    changedDF.show
+    changedDF.write.options(
+        Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).mode("append").kudu
+
+
+    val newDF = sqlContext.read.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu
+    newDF.show
+    val collected = newDF.filter("key = 100").collect()
+    assertEquals("abc", collected(0).getAs[String]("c2_s"))
+
+    deleteRow(100)
+  }
+
+  test("insertion multiple") {
+    val df = sqlContext.read.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu
+    val changedDF = df.limit(2).withColumn("key", df("key").plus(100)).withColumn("c2_s", lit("abc"))
+    changedDF.show
+    changedDF.write.options(
+        Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).mode("append").kudu
+
+
+    val newDF = sqlContext.read.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu
+    newDF.show
+    val collected = newDF.filter("key = 100").collect()
+    assertEquals("abc", collected(0).getAs[String]("c2_s"))
+
+    val collectedTwo = newDF.filter("key = 101").collect()
+    assertEquals("abc", collectedTwo(0).getAs[String]("c2_s"))
+
+    deleteRow(100)
+    deleteRow(101)
+  }
+
+  test("update row") {
+    val df = sqlContext.read.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu
+    val baseDF = df.limit(1) // filter down to just the first row
+    baseDF.show
+    // change the c2 string to abc and update
+    val changedDF = baseDF.withColumn("c2_s", lit("abc"))
+    changedDF.show
+    changedDF.write.options(
+        Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).mode("overwrite").kudu
+
+    //read the data back
+    val newDF = sqlContext.read.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu
+    newDF.show
+    val collected = newDF.filter("key = 0").collect()
+    assertEquals("abc", collected(0).getAs[String]("c2_s"))
+
+    //rewrite the original value
+    baseDF.withColumn("c2_s", lit("0")).write.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).mode("overwrite").kudu
+  }
+
+  test("out of order selection") {
+    val df = sqlContext.read.options(
+      Map("kudu.table" -> tableName, "kudu.master" -> miniCluster.getMasterAddresses)).kudu.select( "c2_s", "c1_i", "key")
+    val collected = df.collect()
+    assert(collected(0).getString(0).equals("0"))
+
   }
 
   test("table scan") {
